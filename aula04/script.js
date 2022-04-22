b435a3f5fde81d5bc70d20f62c82be0c61369609
Jefferson-Guirra <jeffersonloop14@gmail.com>
2022-04-22 20:55:11 -0300

    atualizando exercícios.

---
+++ aula04/script.js
@@ -0,0 +1,3 @@
+window.alert('Minha primeira mensagem')//janela de exibição do alerta.
+        window.confirm('Está gostando de JS?')//janela com botão ok e cancelar.
+        window.prompt('Qual é o seu nome?')//vai perguntar o nome.
