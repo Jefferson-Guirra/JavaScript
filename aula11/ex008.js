a15ef0419dff0122fac809adcfd509dcc70eb588
Jefferson-Guirra <jeffersonloop14@gmail.com>
2022-04-24 16:53:45 -0300

    atividades com JavaScript

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+
+var vel=30
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if(vel>75){
+    console.log('Excedeu o limite permitido nessa via, você foi MULTADO!')
+}
+
+console.log('Dirija com cuidado, use sempre o cinto de segurança')
---
+++ aula11/ex009.js
@@ -0,0 +1,10 @@
+var pais='BRASIL'
+var pais=pais.toLowerCase()
+
+console.log(`Vivendo em ${pais}`)
+if(pais=='brasil'){
+    console.log('você é Brasileiro')
+}
+else{
+    console.log('você é estrangeiro')
+}
