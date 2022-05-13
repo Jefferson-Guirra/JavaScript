9a6856bdf1ed429bd5273299dfd256932ed11757
Jefferson-Guirra <jeffersonloop14@gmail.com>
2022-05-13 20:47:57 -0300

    atividades.

--- Arrays-e-loops/script.js
+++ Arrays-e-loops/script.js
@@ -10,4 +10,47 @@ var c = 0
 while (c<=10){
   console.log(c)
   c++
+
 }
+var nome = ['mirela','jefferson','elaine']
+nome.forEach(function(item,index){
+  console.log(index,item)
+})
+
+for (let i = 0;i<nome.length;i++){
+  console.log(nome[i])
+  if (nome[i]==='jefferson'){
+    break
+  }
+}
+console.log('jefferson'.length)
+var numero = 0
+var maximo= 25
+for (;numero<=maximo;){
+  console.log(numero)
+  numero++
+}
+
+//Array Brasil copa
+
+var Brasil=[1959,1962,1970,1994,2002]
+
+for (let i =0;i < Brasil.length;i++){
+  console.log(Brasil[i])
+}
+
+Brasil.forEach(function(item,index){
+  console.log(`${1+index}- O Brasil ganhou a copa de ${item}`)
+})
+
+var frutas = ['maça','uva','goiaba','laranja','tangerina']
+
+for (let i = 0;i<=frutas.length;i++){
+  console.log(frutas[i])
+  if(frutas[i]==='laranja'){
+    break
+  }
+}
+
+var fruta = frutas[frutas.length-1]
+console.log(fruta)
---
+++ Atribuição-e-ternario/script.js
@@ -0,0 +1,16 @@
+var numero = 20
+numero += 10
+console.log(numero)
+
+var nome = 'mirela'
+var idade=21
+var identidade= (nome==='mirela' && idade>=20) ? 'Identificamos mirela':'não identificamos'
+console.log(identidade)
+
+var possuicasa = true
+var possuicarro = true
+var darcredito
+
+var credito= (possuicasa && possuicarro) ? darcredito = true : 'Não possui os requisitos para crédito'
+
+console.log(darcredito)
