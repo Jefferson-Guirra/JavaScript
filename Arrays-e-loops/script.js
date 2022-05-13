8b9f12f6e05763226f9a05c8d7d50662d5d76e0a
Jefferson-Guirra <jeffersonloop14@gmail.com>
2022-05-13 16:53:07 -0300

    atividades

--- Arrays-e-loops/script.js
+++ Arrays-e-loops/script.js
@@ -1,8 +1,13 @@
-/*var videogames = ['ps4','xbox','ps1']
-console.log(videogames.pop())*/
+var videogames = ['ps4','xbox','ps1']
+console.log(videogames.pop())
 
 //contar de um a 10
 
 for (let c = 0 ;c <= 10;c++){
   console.log(c)
+}
+var c = 0
+while (c<=10){
+  console.log(c)
+  c++
 }
