383fd8f6fc4b89210ee159b042377d4d52151c9d
Jefferson-Guirra <jeffersonloop14@gmail.com>
2022-05-21 20:09:18 -0300

    atividades

---
+++ Objetos/ex001/script.js
@@ -0,0 +1,23 @@
+/*function Pessoa (nome,sobrenome,idade){
+  this.nome = nome
+  this.sobrenome= sobrenome
+  this.idade = idade
+}
+
+Pessoa.prototype.nomeCompleto = function(){
+ return `${this.nome} ${this.sobrenome} ${this.idade}`
+}
+
+
+const marcia = new Pessoa('Marcia','Fragoso',22)
+
+console.log(marcia.nomeCompleto())*/
+
+
+//Acensando a lista de metodos da nodeList e da Htmlcollection
+/*Object.getOwnPropertyNames(NodeList)
+
+Object.getOwnPropertyNames(HTMLCollection)*/
+
+const li = document.querySelector('li')
+console.log(li)
