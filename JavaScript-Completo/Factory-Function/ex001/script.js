fe3cd014bfd0c2f0ddffdf4deb06dbc151e322a9
Jefferson-Guirra <jeffersonloop14@gmail.com>
2022-06-09 21:14:50 -0300

    Factory Function.

---
+++ JavaScript-Completo/Factory-Function/ex001/script.js
@@ -0,0 +1,33 @@
+function createElement(text,elemento){
+  function element (){
+    const newElement = document.createElement(elemento)
+    newElement.innerText = text
+    return newElement
+  }
+  return{
+    text :text,
+    element:element
+  }
+}
+const button =createElement('criado','div')
+console.log(button.element())
+
+/*function test (nome,idade){
+  function andou(){
+    return console.log('andou')
+  }
+
+  function pulou(){
+    return console.log('pulou')
+  }
+  return{
+    nome,
+    idade,
+    andou,
+    pulou
+  }
+
+}
+
+const teste = test('jefferson',24)
+console.log(teste.nome)
