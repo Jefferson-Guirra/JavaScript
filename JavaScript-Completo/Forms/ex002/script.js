f697b67c19ebcdf04cdf1b57f7ee6a85977dd0af
Jefferson Guirra <jeffersonloop14@gmail.com>
2022-06-24 12:47:16 -0300

    Button-Creator.

--- JavaScript-Completo/Forms/ex002/script.js
+++ JavaScript-Completo/Forms/ex002/script.js
@@ -1,6 +1,71 @@
-const controles = document.querySelector('#controles')
+const controles = document.getElementById('controles')
 const cssText = document.querySelector('.css')
 const btn = document.querySelector('.btn')
+controles.addEventListener('change', handleChange)
+
+const handleStyle = {
+  element: btn,
+  backgroundColor(value) {
+    this.element.style.backgroundColor = value
+  },
+  height(value) {
+    this.element.style.height = value + 'px'
+  },
+  width(value) {
+    this.element.style.width = value + 'px'
+  },
+  texto(value) {
+    this.element.innerText = value
+  },
+  color(value) {
+    this.element.style.color = value
+  },
+  border(value) {
+    this.element.style.border = value
+  },
+  borderRadius(value) {
+    this.element.style.borderRadius = value + 'px'
+  },
+  fontFamily(value) {
+    this.element.style.fontFamily = value
+  },
+  fontSize(value) {
+    this.element.style.fontSize = value + 'rem'
+  }
+}
+
+function handleChange(event) {
+  const name = event.target.name
+  const value = event.target.value
+
+  handleStyle[name](value)
+  saveValues(name, value)
+  showCss()
+}
+
+function saveValues(name, value) {
+  localStorage[name] = value
+}
+
+function setValues() {
+  const properties = Object.keys(localStorage)
+  properties.forEach(propertie => {
+    handleStyle[propertie](localStorage[propertie])
+    controles.elements[propertie].value = localStorage[propertie]
+  })
+  showCss()
+}
+setValues()
+
+function showCss() {
+  cssText.innerHTML =
+    '<span>' + btn.style.cssText.split('; ').join(';</span><span>')
+}
+
+/*const controles = document.querySelector('#controles')
+const cssText = document.querySelector('.css')
+const btn = document.querySelector('.btn')
+controles.addEventListener('change',handleChanges)
 
 
 const handleStyle ={
@@ -43,6 +108,7 @@ const handleStyle ={
 function handleChanges(event){
  const name = event.target.name
  const value = event.target.value
+ 
  handleStyle[name](value)
   showCss()
   saveValues(name,value)
@@ -54,7 +120,10 @@ function saveValues(name,value){
 
 function setValues(){
   const properties = Object.keys(localStorage)
+  
+  
   properties.forEach(propertie=>{
+
     handleStyle[propertie](localStorage[propertie])
     controles.elements[propertie].value = localStorage[propertie]
   })
@@ -65,6 +134,4 @@ setValues()
 function showCss(){
   
   cssText.innerHTML ='<span>' + btn.style.cssText.split('; ').join(';</span><span>')
-}
-controles.addEventListener('change',handleChanges)
-
+}*/
